2f358cf0098647d7020dcac12a28d0c0e92b35fa
Jacob Kiggins <=>
2016-09-08 00:18:21 -0400

    Wrote tests for Question model. On tutorial 5

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+import datetime
+
+from .models import Question
+
+class QuestionMethodTest(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        '''
+        was_published_recently() should return False for questions whoes pub_date is in the future.
+        '''
+
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+
+        self.assertEqual(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        '''
+        was_published_recently() should return False for questions > 1 day old
+        '''
+        time = timezone.now() - datetime.timedelta(days=30)
+
+        past_question = Question(pub_date=time)
+
+        self.assertEqual(past_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        '''
+        was_published_recently() should return True for questions < 1 day old
+        '''
+
+        time = timezone.now() - datetime.timedelta(hours=1)
+
+        recent_question = Question(pub_date=time)
+
+        self.assertEqual(recent_question.was_published_recently(), True)
+
+
