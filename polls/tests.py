a0277fa0eabb23ac80a35dbf938b9cc33f7149aa
Jacob Kiggins <=>
2016-09-09 11:27:48 -0400

    wrote view tests, ready to hand in

--- polls/tests.py
+++ polls/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.utils import timezone
+from django.core.urlresolvers import reverse
 
 import datetime
 
@@ -36,5 +37,59 @@ class QuestionMethodTest(TestCase):
         recent_question = Question(pub_date=time)
 
         self.assertEqual(recent_question.was_published_recently(), True)
+
+
+def create_question(question_text, days):
+    '''Creates a question days in the future'''
+
+    time = timezone.now() + datetime.timedelta(days=days)
+
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+class QuestionViewTests(TestCase):
+    def test_index_view_with_no_questions(self):
+        '''If no questions exist, the appropirate message should be displayed'''
+        response = self.client.get(reverse('polls:index'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_index_view_with_a_past_question(self):
+        '''Questions with a pub_date in the past should be displayed'''
+        create_question(question_text="Past Question.", days=-30)
+
+        response=self.client.get(reverse('polls:index'))
+
+        self.assertQuerysetEqual(response.context['latest_question_list'], ['<Question: Past Question.>'])
+
+    def test_index_view_with_a_future_question(self):
+        '''Questions with a pub_date in the future should not be displayed'''
+        create_question(question_text="This is a future question", days=30)
+
+        response = self.client.get(reverse('polls:index'))
+
+        self.assertContains(response, "No polls are available")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_index_view_with_future_question_and_past_question(self):
+        '''Make sure past question is displayed and future question isn't'''
+        create_question(question_text="Past Question.", days=-30)
+        create_question(question_text="This is a future question", days=30)
+
+        response = self.client.get(reverse('polls:index'))
+
+        self.assertQuerysetEqual(response.context['latest_question_list'], ['<Question: Past Question.>'])
+
+    def test_index_view_with_two_past_questions(self):
+        '''Make sure two past questions are displayed and ordered'''
+        create_question(question_text="Past Question 2", days=-30)
+        create_question(question_text="Past Question 1", days=-20)
+
+        response = self.client.get(reverse('polls:index'))
+
+        self.assertQuerysetEqual(response.context['latest_question_list'], ['<Question: Past Question 1>', '<Question: Past Question 2>'])
+
 
 
--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,8 @@ from django.core.urlresolvers import reverse
 from django.db.models.expressions import F
 from django.views import generic
 
+from django.utils import timezone
+
 from .models import Question, Choice
 
 class IndexView(generic.ListView):
@@ -13,7 +15,8 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """"Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
 
 class DetailView(generic.DetailView):
     model = Question
